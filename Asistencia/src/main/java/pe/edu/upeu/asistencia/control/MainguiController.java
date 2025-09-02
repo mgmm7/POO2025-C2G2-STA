b4b554c8c8762b711333e10e282b761c0bd437ea
PedroLoa25 <pfloayza1704@gmail.com>
2025-09-02 12:14:01 -0500

    cambio casi final

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -1,15 +1,21 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TabPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import pe.edu.upeu.asistencia.Aplication;
 
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 
@@ -22,20 +28,46 @@ public class MainguiController {
     TabPane tabPane;
     @FXML
     MenuItem MenuItem1, MenuItemC;
+    @Autowired
+    ApplicationContext context;
 
     public void initialize() {
-
+        MenuItemListener mIL=new MenuItemListener();
+        MenuItem1.setOnAction(mIL::handle);
+        MenuItemC.setOnAction(mIL::handle);
     }
 
     class MenuItemListener{
-        public void handle(ActionEvent e){//STA
+        Map<String, String[]> menuConfig=Map.of(
+                "MenuItem1", new String[]{"/fxml/main_participante.fxml","Reg.Participante","T"},
+                "MenuItemC", new String[]{"/fxml/login.fxml","salir","C"}
+        );
 
+        public void handle(ActionEvent e){//STA
+                String id=((MenuItem)e.getSource()).getId();
+                if (menuConfig.containsKey(id)){
+                    String[] items=menuConfig.get(id);
+                    if(items[2].equals("C")){
+                        Platform.exit();
+                        System.exit(0);
+                    }else{
+                        abrirTabPaneFXML(items[0],items[1]);
+                    }
+                }
         }
         private void abrirTabPaneFXML(String fxmlPath,String tittle){
             try {
-
-            }catch (Exception ex){
-                System.out.println(ex.getMessage());
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+                fxmlLoader.setControllerFactory(context::getBean);
+                Parent root = fxmlLoader.load();
+                ScrollPane scrollPane = new ScrollPane(root);
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+                Tab newtab = new Tab(tittle, scrollPane);
+                tabPane.getTabs().clear();
+                tabPane.getTabs().add(newtab);
+            }catch (IOException ex){
+                throw new RuntimeException(ex);
             }
 
         }
